d69a99d8b5f63e861c38af6bd0a6dd3df4c3336f
DanielB-02 <daniel.d.bor@gmail.com>
2024-04-30 13:17:52 +0200

    Added the necessary models for creating contracts. Also added and the tested the EditForms to see if information is stored in the models.

--- Ipsen5_groep01_frontend/Components/Model/DossierModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ipsen5_groep01_frontend.Components.Data;
-
-public class DossierModel
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string EmailAddress { get; set; }
-}
