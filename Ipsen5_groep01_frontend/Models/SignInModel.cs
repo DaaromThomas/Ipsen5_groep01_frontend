526d887ab2ec4e4ca9886aff6c0db93b3b9278cb
Zakaria Ben Moussa <holeeszak@outlook.com>
2024-05-01 14:32:52 +0200

    feat: Added signIn functionality

---
+++ Ipsen5_groep01_frontend/Models/SignInModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Ipsen5_groep01_frontend.Models
+{
+    public class SignInModel
+    {
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
---
+++ Ipsen5_groep01_frontend/Services/RequestMakerService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipsen5_groep01_frontend.Services
+{
+    public class RequestMakerService
+    {
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "http://localhost:5196/api"; 
+
+        public RequestMakerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<HttpResponseMessage> MakeRequest(HttpMethod method, string endpoint, HttpContent content = null)
+        {
+            var url = $"{BaseUrl}/{endpoint}"; 
+            var request = new HttpRequestMessage(method, url)
+            {
+                Content = content
+            };
+
+            return await _httpClient.SendAsync(request);
+        }
+
+        public async Task<HttpResponseMessage> MakePostRequest<T>(string endpoint, T data)
+        {
+            var url = $"{BaseUrl}/{endpoint}"; 
+            var json = JsonConvert.SerializeObject(data);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return await _httpClient.PostAsync(url, content);
+        }
+    }
+}
