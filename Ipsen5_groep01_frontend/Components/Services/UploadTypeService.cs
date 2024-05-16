d0835e96a699149c7884db331a32126c2301fb97
MarcelDaniels <s1135718@student.hsleiden.nl>
2024-05-16 11:44:43 +0200

    feat: sending document to backend api

--- Ipsen5_groep01_frontend/Components/Services/UploadTypeService.cs
+++ Ipsen5_groep01_frontend/Components/Services/UploadTypeService.cs
@@ -1,9 +1,18 @@
+using System.Text;
 using Ipsen5_groep01_frontend.Models;
 
 namespace Ipsen5_groep01_frontend.Components.Services
 {
     public class UploadTypeService
     {
+        private readonly HttpClient httpClient;
+
+        public UploadTypeService()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:5196/api/");
+            
+        }
         private List<UploadType> documenten = new List<UploadType>
         {
             new UploadType { Type = "Rijbewijs", DocumentType = "Pdf", Required = false },
@@ -24,6 +33,24 @@ namespace Ipsen5_groep01_frontend.Components.Services
         public void VerwijderDocument(UploadType document)
         {
             documenten.Remove(document);
+        }
+        public async Task ToevoegenDocumentAsync(UploadType uploadType)     
+        {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(uploadType);
+
+            Console.WriteLine("Verzonden JSON-gegevens naar backend:");
+            Console.WriteLine(json);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("uploadtype/adduploadtype", content);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Document succesvol toegevoegd!");
+            }
+            else
+            {
+                Console.WriteLine("Er is een fout opgetreden bij het toevoegen van het document.");
+                Console.WriteLine(response);
+            }
         }
     }
 }
