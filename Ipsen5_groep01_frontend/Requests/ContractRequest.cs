cab32e764bb63c5a050636dcb0c7cbb5797b60c0
Zakaria Ben Moussa <holeeszak@outlook.com>
2024-05-21 12:16:50 +0200

    feat: Add customer config

---
+++ Ipsen5_groep01_frontend/Requests/ContractRequest.cs
@@ -0,0 +1,9 @@
+﻿using Ipsen5_groep01_frontend.Models;
+
+namespace Ipsen5_groep01_frontend.Requests
+{
+    public class ContractRequest
+    {
+        public Contract ContractDto { get; set; }
+    }
+}
