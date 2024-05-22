1f4733508adf142500f58e1565aebf72d2b9ddf9
Zakaria Ben Moussa <holeeszak@outlook.com>
2024-05-22 18:54:00 +0200

    feat: added authguard

---
+++ Ipsen5_groep01_frontend/Services/AuthService.cs
@@ -0,0 +1,27 @@
+﻿namespace Ipsen5_groep01_frontend.Services
+{
+    public class AuthService
+    {
+        private bool _isLoggedIn = false;
+        private string _role = string.Empty;
+
+        public bool IsLoggedIn => _isLoggedIn;
+        public string Role => _role;
+
+        public void LogIn(string role)
+        {
+            _isLoggedIn = true;
+            _role = role;
+
+        }
+
+        public void LogOut()
+        {
+            _isLoggedIn = false;
+            _role = string.Empty;
+        }
+
+       
+
+    }
+}
