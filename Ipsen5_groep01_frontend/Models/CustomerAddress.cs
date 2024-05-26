83bf872ecc81c0317659422cf67cfa62a0032614
Zakaria Ben Moussa <holeeszak@outlook.com>
2024-05-26 14:48:34 +0200

    feat: Added field != null validation to customconfigfields

--- Ipsen5_groep01_frontend/Models/CustomerAddress.cs
+++ Ipsen5_groep01_frontend/Models/CustomerAddress.cs
@@ -1,11 +1,15 @@
-﻿namespace Ipsen5_groep01_frontend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ipsen5_groep01_frontend.Models
 {
     public class CustomerAddress
     {
         public string StreetName { get; set; }
 
+        [Required(ErrorMessage = "Huisnummer is verplicht")]
         public string HouseNumber { get; set; }
 
+        [Required(ErrorMessage = "Postcode is verplicht")]
         public string PostalCode { get; set; }
 
         public string Country { get; set; }
