d581bc7d3ecac70eb7db0b58bd13a0ab09642e12
DanielB-02 <daniel.d.bor@gmail.com>
2024-06-01 20:38:00 +0200

    feat: candidaat model toegevoegd.

--- Ipsen5_groep01_frontend/Models/Candidate.cs
+++ Ipsen5_groep01_frontend/Models/Candidate.cs
@@ -2,5 +2,17 @@
 
 public class Candidate
 {
+    public Guid Id { get; set; } 
+
+    public string FirstName { get; set; } 
+
+    public string LastName { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public string BSN { get; set; }
+
+    public DateTime CreatedDate { get; set; }
     
+    public DateTime UpdatedDate { get; set; }
 }
