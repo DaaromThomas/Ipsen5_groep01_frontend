c216ba3f324b424ac48a33c2c4a4dae568295e11
Zakaria Ben Moussa <holeeszak@outlook.com>
2024-06-02 13:35:22 +0200

    refactor: rewirted viewCustomerconfig to uploadservice and customerservice

---
+++ Ipsen5_groep01_frontend/Services/UploadService.cs
@@ -0,0 +1,32 @@
+﻿using Ipsen5_groep01_frontend.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Ipsen5_groep01_frontend.Services
+{
+    public class UploadService
+    {
+        private readonly RequestMakerService _requestMakerService;
+
+        public UploadService(RequestMakerService requestMakerService)
+        {
+            _requestMakerService = requestMakerService;
+        }
+
+        public async Task<List<UploadTypeModel>> GetUploadTypesByCustomerId(Guid customerId)
+        {
+            var response = await _requestMakerService.MakeGetRequest($"uploadtype/customerid/{customerId}");
+            var json = await response.Content.ReadAsStringAsync();
+
+            var outerObject = JObject.Parse(json);
+            var uploadTypeArray = outerObject["result"]["uploadTypeDto"];
+
+            return uploadTypeArray.Select(jToken => new UploadTypeModel
+            {
+                Id = Guid.Parse((string)jToken["id"]),
+                Type = (string)jToken["type"],
+                Required = (bool)jToken["required"]
+            }).ToList();
+        }
+    }
+
+}
