1bb027c686cba5c3e8525894ca1760097bf9d85c
Zakaria Ben Moussa <holeeszak@outlook.com>
2024-06-04 14:03:06 +0200

    feat: added candidate service for dispay candidateName

---
+++ Ipsen5_groep01_frontend/Services/CandidateService.cs
@@ -0,0 +1,41 @@
+﻿using Ipsen5_groep01_frontend.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Ipsen5_groep01_frontend.Services
+{
+    public class CandidateService
+    {
+        private readonly RequestMakerService _requestMakerService;
+
+        public CandidateService(RequestMakerService requestMakerService)
+        {
+            _requestMakerService = requestMakerService;
+        }
+
+
+        public async Task<Candidate> GetCandidateById(Guid candidateId)
+        {
+            var response = await _requestMakerService.MakeGetRequest($"Candidate/candidatebyid/{candidateId}");
+            var json = await response.Content.ReadAsStringAsync();
+
+            var outerObject = JObject.Parse(json);
+            var candidateArray = outerObject["result"]["candidateDto"];
+
+            return ParseCandidate(candidateArray);
+        }
+
+        private Candidate ParseCandidate(JToken jToken)
+        {
+            var candidate = new Candidate
+            {
+                Id = Guid.Parse((string)jToken["id"]),
+                FirstName = (string)jToken["firstName"],
+                LastName = (string)jToken["lastName"],
+                BSN = (string)jToken["bsn"],
+                
+            };
+
+            return candidate;
+        }
+    }
+}
