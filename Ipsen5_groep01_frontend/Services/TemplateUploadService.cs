477045551dd168ee161cd3859bf93a0d253f7882
ThomasPijper <56thomasp56@gmail.com>
2024-06-11 14:11:27 +0200

    Fix: updated BinaryDocument and put the function in the config page

--- Ipsen5_groep01_frontend/Services/TemplateUploadService.cs
+++ Ipsen5_groep01_frontend/Services/TemplateUploadService.cs
@@ -1,8 +1,34 @@
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+
 namespace Ipsen5_groep01_frontend.Services{
     public class TemplateUploadService{
         private List<BinaryDocument> _documents = [];
-
         private Guid _customerId;
+        private readonly HttpClient _httpClient;
+
+        public TemplateUploadService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            httpClient.BaseAddress = new Uri("http://localhost:5196/api/");
+        }
+
+        public async Task UploadAllDocuments(){
+            foreach(BinaryDocument document in _documents){
+                _uploadDocument(document);
+            }
+        }
+
+        private async Task<string> _uploadDocument(BinaryDocument document){
+            var formData = new MultipartFormDataContent();
+            formData.Add(new StreamContent(document.GetStream()), "file", document.GetFileName());
+
+            Console.WriteLine(formData);
+            var response = await _httpClient.PostAsync($"BlobStorage/uploadTemplate/{_customerId}/{document.GetUploadTypeName()}", formData);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
 
         public void ClearDocuments(){
             _documents.Clear();
@@ -32,28 +58,38 @@ namespace Ipsen5_groep01_frontend.Services{
     }
 
     public class BinaryDocument{
-        private byte[] _bytes;
+        private Stream _stream;
         private string _fileName;
+        private Guid _uploadTypeName;
 
-        public BinaryDocument(byte[] bytes, string name){
-            _bytes = bytes;
+        public BinaryDocument(Stream stream, string name, Guid uploadTypeName){
+            _stream = stream;
             _fileName = name;
+            _uploadTypeName = uploadTypeName;
         }
 
         public string GetFileName(){
             return _fileName;
         }
 
-        public byte[] GetBytes(){
-            return _bytes;
+        public Stream GetStream(){
+            return _stream;
+        }
+
+        public Guid GetUploadTypeName(){
+            return _uploadTypeName;
         }
 
         public void SetFileName(string name){
             _fileName = name;
         }
 
-        public void SetBytes(byte[] bytes){
-            _bytes = bytes;
+        public void SetStream(Stream stream){
+            _stream = stream;
+        }
+
+        public void SetUploadTypeName(Guid uploadTypeName){
+            _uploadTypeName = uploadTypeName;
         }
     }
 
